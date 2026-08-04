b46721baaad4371fda22f7f209cd72e82badf5f3
Jemi Yosua Laoere <36179410+jemiyosua@users.noreply.github.com>
2026-08-04 07:36:44 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -828,7 +828,7 @@ function Register() {
 
 export default function NexusClashApp() {
   const [page, setPage] = useState("home");
-  const [showBracket, setShowBracket] = useState(false);
+  const [showBracket, setShowBracket] = useState(true);
 
   const go = useCallback((p) => {
     setPage(p);
